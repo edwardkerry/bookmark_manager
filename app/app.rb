74188d00ed92083b4c14ad3435a620a696665e8a
edwardkerry <edwardkerry@gmail.com>
2015-12-21 12:02:06 +0000

    Controllers and partials refactored

--- app/app.rb
+++ app/app.rb
@@ -2,82 +2,11 @@ ENV['RACK_ENV'] ||= 'development'
 
 require 'sinatra/base'
 require 'sinatra/flash'
+require 'sinatra/partial'
+
 require_relative 'data_mapper_setup'
-
-class BookmarkManager < Sinatra::Base
-
-  use Rack::MethodOverride
-
-  enable :sessions
-  set :session_secret, 'super secret'
-  register Sinatra::Flash
-
-  get '/links' do
-    @links = Link.all
-    erb :'links/index'
-  end
-
-  get '/links/new' do
-    erb :'links/new'
-  end
-
-  post '/links' do
-    link = Link.create(title: params[:title], url: params[:url])
-    params[:tags].split.each do |tag|
-      tag = Tag.first_or_create(name: tag)
-      link.tags << tag
-    end
-    link.save
-    redirect '/links'
-  end
-
-  get '/tags/:name' do
-    tag = Tag.first(name: params[:name])
-    @links = tag ? tag.links : []
-    erb :'links/index'
-  end
-
-  get '/users/new' do
-    @user = User.new
-    erb :'users/new'
-  end
-
-  post '/users' do
-    @user = User.new(email: params[:email], password: params[:password], password_confirmation: params[:password_confirmation])
-    if @user.save
-      session[:user_id] = @user.id
-      redirect '/links'
-    else
-      flash.now[:errors]= @user.errors.full_messages
-      erb :'users/new'
-    end
-  end
-
-  get '/sessions/new' do
-    erb :'sessions/new'
-  end
-
-  post '/sessions' do
-    @user = User.authenticate(params[:email], params[:password])
-    if @user
-      session[:user_id] = @user.id
-      redirect '/links'
-    else
-      flash.now[:errors] = @user.errors.full_messages
-      erb :'sessions/new'
-    end
-  end
-
-  delete '/sessions' do
-    session[:user_id] = nil
-    flash.keep[:notice] = 'goodbye!'
-    redirect '/links'
-  end
-
-  helpers do
-    def current_user
-      @current_user ||= User.get(session[:user_id])
-    end
-  end
-
-end
+require_relative 'server'
+require_relative 'controllers/links'
+require_relative 'controllers/tags'
+require_relative 'controllers/sessions'
+require_relative 'controllers/users'
---
+++ app/controllers/links.rb
@@ -0,0 +1,22 @@
+class BookmarkManager < Sinatra::Base
+
+  get '/links' do
+    @links = Link.all
+    erb :'links/index'
+  end
+
+  get '/links/new' do
+    erb :'links/new'
+  end
+
+  post '/links' do
+    link = Link.create(title: params[:title], url: params[:url])
+    params[:tags].split.each do |tag|
+      tag = Tag.first_or_create(name: tag)
+      link.tags << tag
+    end
+    link.save
+    redirect '/links'
+  end
+
+end
